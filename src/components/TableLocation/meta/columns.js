276718e416f69e23197d435334826c6c6b72cf3d
romain-ngo <ngo.romain@gmail.com>
2019-09-12 09:44:53 +0800

    Added sorter to column

--- src/components/TableLocation/meta/columns.js
+++ src/components/TableLocation/meta/columns.js
@@ -3,16 +3,19 @@ const columns = [
     title: 'Country',
     dataIndex: 'country',
     key: 'country',
+    sorter: (a, b) => a.country.localeCompare(b.country)
   },
   {
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
+    sorter: (a, b) => a.name.localeCompare(b.name)
   },
   {
     title: 'Temperature',
     dataIndex: 'temperature',
     key: 'temperature',
+    sorter: (a, b) => a.temperature - b.temperature
   },
   {
     title: 'Condition',
